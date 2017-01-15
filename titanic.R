ee6a0266ff7a594ea8c5337279f3cdae53ffe361
Pete Deevakul <editor@mozzarella.website>
2017-01-15 01:58:35 -0500

    rename script

--- titanic.R
+++ titanic.R
@@ -27,7 +27,7 @@ titanic$sex <- factor(titanic$sex)
 ggplot(titanic, aes(pclass, fill = sex)) +
   geom_bar(position = "dodge") +
   scale_fill_manual(values = c( "deepskyblue4", "bisque2")) +
-  theme_minimal(base_size = 14, base_family = "GillSans") +
+  theme_gray(base_size = 14, base_family = "GillSans") +
   theme(plot.margin = unit(c(1, 1, 1, 1), "cm"),
         axis.title.x = element_text(family = "Times", face = "italic"),
         axis.title.y = element_text(family = "Times", face = "italic")) +
@@ -43,21 +43,21 @@ ggplot(titanic, aes(pclass, fill = sex)) +
         axis.title.x = element_text(family = "Times", face = "italic"),
         axis.title.y = element_text(family = "Times", face = "italic")) +
   facet_grid(. ~ survived) +
-  labs(title = "Titanic Survivors: Passenger Class ~ Gender", 
+  labs(title = "Survivors: Passenger Class ~ Gender", 
        x = "class", y = "count", fill = "gender")
 
 # define a jittered position
 posn.j <- position_jitter(0.5, 0)
 
 # Use ggplot() for the last instruction
-ggplot(titanic, aes(pclass, age, col = sex)) +
+ggplot(titanic, aes(pclass, age, color = sex)) +
   geom_jitter(size = 3, alpha = 0.65, position = posn.j, 
               aes(shape = sex)) +
-  scale_color_manual(values = c("deepskyblue4", "bisque3")) +
+  scale_color_manual(values = c("deepskyblue4", "firebrick3")) +
   theme_gray(base_size = 14, base_family = "GillSans") +
   theme(plot.margin = unit(c(1, 1, 1, 1), "cm"),
         axis.title.x = element_text(family = "Times", face = "italic"),
         axis.title.y = element_text(family = "Times", face = "italic")) +
   facet_grid(. ~ survived) +
-  labs(title = "Titanic Survivors: Passenger Class ~ Gender", 
-       x = "class", y = "count", fill = "gender")
+  labs(title = "Survivors: Passenger Class ~ Gender", 
+       x = "class", y = "count")
